9636dc9a5eba00e9ded94e58f2a0b37bf571b7f6
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 17:40:13 -0500

    created accountdao

--- src/main/java/com/revature/repo/DaoContract.java
+++ src/main/java/com/revature/repo/DaoContract.java
@@ -2,7 +2,7 @@ package com.revature.repo;
 
 import java.util.List;
 
-/*
+/**
  * @auther clifton ferguson
  * 
  * @param <T> Type of model
@@ -13,7 +13,7 @@ import java.util.List;
  */
 
 public interface DaoContract <T, I> {
-	/*
+	/**
 	 * 
 	 * @return A list of all instances in the db
 	 *  
@@ -21,34 +21,31 @@ public interface DaoContract <T, I> {
 	
     
 	List<T> findAll();
-	/*
+	/**
 	 * 
 	 * @param i primary key of the instance
 	 * @return the instance with the same primary key
 	 */
 	
 	T findById(I i);
-	/*
+	/**
 	 * @param t the instance to update
 	 * @return the updated instance
 	 */
 	
 	T update(T t);
 	
-	/*
+	/**
 	 * @param t the instance to create
 	 * @return the created instance
 	 */
 	
 	T create(T t);
 	
-	/*
+	/**
 	 * @param i the primary key of the instance to be removed
 	 * @return how many were removed [0 if failure, 1 if success]
 	 */
 	
 	int delete(I i);
-	
-	
-	
 }
