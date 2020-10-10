71dbca998e731e4d5a5175fbf8f77d3d2208d0db
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 15:37:46 -0500

    Finished Model for Activity and Account

--- src/main/java/com/revature/model/Account.java
+++ src/main/java/com/revature/model/Account.java
@@ -1,5 +1,74 @@
 package com.revature.model;
 
-public class Account {
+import java.util.Set;
 
+public class Account {
+	//create private fields from SQL table
+    //generate getters and setters
+	//generate toString()
+	//generate constructors
+	
+	private int account_id;
+	private String username;
+	private String password;
+	private float current_balance;
+	private boolean is_customer;
+	private Set<Activity> activity;
+	
+	public int getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public float getCurrent_balance() {
+		return current_balance;
+	}
+	public void setCurrent_balance(float current_balance) {
+		this.current_balance = current_balance;
+	}
+	public boolean isIs_customer() {
+		return is_customer;
+	}
+	public void setIs_customer(boolean is_customer) {
+		this.is_customer = is_customer;
+	}
+	public Set<Activity> getActivity() {
+		return activity;
+	}
+	public void setActivity(Set<Activity> activity) {
+		this.activity = activity;
+	}
+	@Override
+	public String toString() {
+		return "Account [account_id=" + account_id + ", username=" + username + ", password=" + password
+				+ ", current_balance=" + current_balance + ", is_customer=" + is_customer + ", activity=" + activity
+				+ "]";
+	}
+	public Account(int account_id, String username, String password, float current_balance, boolean is_customer,
+			Set<Activity> activity) {
+		super();
+		this.account_id = account_id;
+		this.username = username;
+		this.password = password;
+		this.current_balance = current_balance;
+		this.is_customer = is_customer;
+		this.activity = activity;
+	}
+	
+	
+	
+	
 }
