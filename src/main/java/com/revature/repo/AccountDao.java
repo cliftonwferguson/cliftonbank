3ba473cad778dbad148ed7ff3289ee1455daefb0
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 17:43:29 -0500

    implemented account methods

--- src/main/java/com/revature/repo/AccountDao.java
+++ src/main/java/com/revature/repo/AccountDao.java
@@ -1,5 +1,39 @@
 package com.revature.repo;
 
-public class AccountDao {
+import java.util.List;
+
+import com.revature.model.Account;
+
+public class AccountDao implements DaoContract <Account, String> {
+
+	@Override
+	public List<Account> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account findById(String i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account update(Account t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account create(Account t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(String i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
