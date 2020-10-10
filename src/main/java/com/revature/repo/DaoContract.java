5f48db192048719023e0ed756d319cabeea6768b
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 16:09:02 -0500

    dao interface half

--- src/main/java/com/revature/repo/DaoContract.java
+++ src/main/java/com/revature/repo/DaoContract.java
@@ -1,5 +1,43 @@
 package com.revature.repo;
 
-public interface DaoContract {
+import java.util.List;
 
+/*
+ * @auther clifton ferguson
+ * 
+ * @param <T> Type of model
+ * @param <I> Primary key for model
+ * 
+ * This will create a contract for all doa classes to
+ * follow.
+ */
+
+public interface DaoContract <T, I> {
+	/*
+	 * 
+	 * @return A list of all instances in the db
+	 *  
+	 */
+	
+    
+	List<T> findAll();
+	/*
+	 * 
+	 * @param i primary key of the instance
+	 * @return the instance wit hthe same primary key
+	 */
+	
+	T findById(I i);
+	/*
+	 * 
+	 */
+	
+	T update(T t);
+	
+	T create(T t);
+	
+	int delete(I i);
+	
+	
+	
 }
