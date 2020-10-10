f64898336ded5926cb0ab66bd5238a1f77f7bab5
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 17:27:39 -0500

    implemented doacontract and imported model

---
+++ src/main/java/com/revature/repo/ActivityDao.java
@@ -0,0 +1,39 @@
+package com.revature.repo;
+
+import java.util.List;
+
+import com.revature.model.Activity;
+
+public class ActivityDao implements DaoContract <Activity, Integer> {
+
+	@Override
+	public List<Activity> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity findById(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity update(Activity t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity create(Activity t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(Integer i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
