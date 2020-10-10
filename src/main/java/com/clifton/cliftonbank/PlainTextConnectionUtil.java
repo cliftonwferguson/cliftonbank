608b0e0c74623924c9a29e2007fd726a063278a8
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 13:12:20 -0500

    Created model package

--- src/main/java/com/clifton/cliftonbank/PlainTextConnectionUtil.java
+++ src/main/java/com/clifton/cliftonbank/PlainTextConnectionUtil.java
@@ -12,33 +12,5 @@ public class PlainTextConnectionUtil {
 	private static final String user = "jdbc_worker";
 	private static final String password = "wasspord";
 
-	public static void main(String[] args) {
-
-		try (Connection conn = DriverManager.getConnection(url, user, password);
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("select * from person;")) {
-
-			// while loop
-
-			while (rs.next()) {
-
-				// mapping data from resultSet into local variables
-
-				
-				String fileExt = rs.getString("email");
-				
-
-				// printing result to console
-				System.out.printf(
-						"file_hash: %s, file_size: %d, file_extension: %s"
-								+ "mime_type: %s, file_tags: %s, file_name: %s%n",
-						hash, fileSize, fileExt, mimeType, fileTags, fileName);
-
-			}
-
-		} catch (SQLException t) {
-			throw new RuntimeException(t);
-		}
-
-	}
+	
 }
