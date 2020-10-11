29836bdbf8132534e26a921b7213d89948f05291
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-10 20:03:21 -0500

    findall sql

--- src/main/java/com/revature/repo/ActivityDao.java
+++ src/main/java/com/revature/repo/ActivityDao.java
@@ -1,15 +1,33 @@
 package com.revature.repo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.clifton.cliftonbank.PlainTextConnectionUtil;
 import com.revature.model.Activity;
 
 public class ActivityDao implements DaoContract <Activity, Integer> {
-
+  
 	@Override
 	public List<Activity> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Activity> activitys = new LinkedList<>();
+		try(Connection conn = PlainTextConnectionUtil.getInstance().getConnection()){
+			Statement s = conn.createStatement();
+			String sql = "select * from Activity";
+			ResultSet rs = s.executeQuery(sql);
+			while(rs.next()) {
+				activitys.add(new Activity(rs.getInt(1), rs.getInt(2), rs.getString(3), null));
+			}
+			rs.close();
+			s.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return activitys;
 	}
 
 	@Override
